3a4402f8a432f44921bf736063bc839ef3682144
Swen Breitung <veroxa@web.de>
2023-06-07 01:26:32 +0200

    js, html and css

--- html.js
+++ html.js
@@ -3,12 +3,12 @@ function loadCardHtml(i, img, name, typeOne, pokemonId) {
     <div class="pokemon_card  cardList hover" onclick="openCardInfo('${img}')" id="pokemon${i}" style="width: 10rem;">
    
        <div  class="card-main background">
-       <div class="card-above">
-       <div>${name}</div>
-       <div>ID: #${pokemonId}</div>
+    <div class="card-above">
+       <div class="m-1 fw-bold">${name}</div>
+       <div class="m-1 fw-bold">ID: #${pokemonId}</div>
    </div>
    <div class="card-below">
-   <div>${typeOne}</div>
+       <div class="m-1 fw-bold">${typeOne}</div>
        <div class="bg-container"></div>
        
        <div class="image-container">
@@ -20,13 +20,13 @@ function loadCardHtml(i, img, name, typeOne, pokemonId) {
 }
 
 function loadHtmlCardInfo(img) {
-    return /*html*/ `<div class="card ">
+    return /*html*/ `<div class="card-info">
     <img src="${img}" class="card-img-top" alt="...">
     <div class="card-body">
         <div>
-            <button></button>
-            <button></button>
-            <button></button>
+        <a href="#" class="btn btn-primary">Stats</a>
+        <a href="#" class="btn btn-primary">Info</a>
+        <a href="#" class="btn btn-primary">Skills</a>
         </div>
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
